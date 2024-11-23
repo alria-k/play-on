4dfa27739fef22cd0228cdbef922bcfa0fdacc9d
Alria <artur.kirovich@gmail.com>
2024-11-23 16:25:07 +0100

    Util #1, MediaBreackpoint function

---
+++ src/utils/MediaBreackpoint.ts
@@ -0,0 +1,13 @@
+enum DeviceScreen {
+    MobileXS = '320px',
+    MobileS = '375px',
+    Mobile = '425px',
+    Tablet = '768px',
+    Laptop = '1024px',
+    LaptopL = '1440px',
+    Desktop = '2560px',
+}
+
+export const mediaBreakpoint = (device: DeviceScreen, rule: 'min' | 'max' = 'min'): string => `
+  @media screen and (${rule}-width: ${device})
+`;
