805557d1a9038f4b431e0865014e81b51983e702
Alria <artur.kirovich@gmail.com>
2024-11-23 16:26:13 +0100

    Page folder created

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import { Header } from "../";
+
+export const HomePage: FunctionComponent = () => {
+    return(
+        <Header/>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+// COMPONENTS
+export { Header } from "../ui";
+
+// PAGES
+export {HomePage} from './HomePage/HomePage'
