8fdd644e0d95e1fe392ca313a7d4bcd5b68c1a17
Alria <artur.kirovich@gmail.com>
2024-12-01 21:47:38 +0100

    finished burger menu functional

--- src/ui/Navigation/Navigation.tsx
+++ src/ui/Navigation/Navigation.tsx
@@ -1,10 +1,14 @@
-import {FunctionComponent} from 'react'
+import {FunctionComponent, MouseEventHandler, ReactElement, useState} from 'react'
 import styled from 'styled-components'
 import { useMediaQuery } from 'usehooks-ts'
 
 import { LinkText } from "../Link/Link"
 import { Colors } from '../'
 import { mediaBreakpoint, DeviceScreen } from '../../utils/mediaBreakpoint'
+
+type isMenuOpenType = {
+    isMenuOpen: boolean;
+}
 
 const Links = [
     {
@@ -25,6 +29,7 @@ const NavigationBox = styled.nav`
     display: flex;
     align-items: center;
     gap: 50px;
+    background-color: ${Colors.BackgroundFill};
     ${mediaBreakpoint(DeviceScreen.Tablet, 'max')} {
         position: absolute;
         left: 0;
@@ -41,20 +46,25 @@ const BurgerContainer = styled.div`
     z-index: 1;
     width: 100%;
 `
-const BurgerButton = styled.div`
+const BurgerButton = styled.button`
     position: relative;
+    outline: none;
+    border: none;
     width: 26px;
     height: 24px;
     background-color: ${Colors.BtnColor1};
+    border-radius: 4px;
+    padding: 0;
 `
-const BurgerMenu = styled.div`
-    display: flex;
+const BurgerMenu = styled.div<isMenuOpenType>`
+    display: ${({isMenuOpen}) => isMenuOpen ? 'flex;' : 'none;'};
     flex-direction: column;
     gap: 15px;
     align-items: center;
-    margin: 24px 0 0 0px
+    margin: 24px 0 0 0px;
+
 `
-const BurgerLine = styled.span`
+const BurgerLine = styled.span<isMenuOpenType>`
     position: absolute;
     display: block;
     width: 20px;
@@ -63,21 +73,25 @@ const BurgerLine = styled.span`
     top: 50%;
     left: 3px;
     border-radius: 4px;
+    transform:${({isMenuOpen}) => isMenuOpen ? 'rotate(45deg);' : 'rotate(0deg);'}
     &::before {
         content: '';
         position: absolute;
         width: 20px;
         height: 1px;
         background-color: ${Colors.TextColor};
-        top: -6px;
+        top: ${({isMenuOpen}) => isMenuOpen ? '0px;' : '-6px;'}
+        left: 0;
+        transform:${({isMenuOpen}) => isMenuOpen ? 'rotate(90deg);' : 'rotate(0deg);'}
     }
     &::after {
         content: '';
         position: absolute;
-        width: 20px;
-        height: 1px;
+        width: ${({isMenuOpen}) => isMenuOpen ? '0': '20px'};
+        height: ${({isMenuOpen}) => isMenuOpen ? '0': '1px'};
         background-color: ${Colors.TextColor};
         top: 6px;
+        left: 0;
     }
 `
 const BurgerButtonBox = styled.div`
@@ -86,18 +100,23 @@ const BurgerButtonBox = styled.div`
 `
 
 export const Navigation: FunctionComponent = () => {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
     const matches = useMediaQuery('(max-width: 768px)')
 
-  return (
+    const menuOpenHandler = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+    return (
     <NavigationBox>
         {matches ? 
         <BurgerContainer>
             <BurgerButtonBox>
-                <BurgerButton>
-                    <BurgerLine />
+                <BurgerButton onClick={menuOpenHandler}>
+                    <BurgerLine isMenuOpen={menuOpen}/>
                 </BurgerButton>
             </BurgerButtonBox>
-            <BurgerMenu>
+            <BurgerMenu isMenuOpen={menuOpen}>
                 {Links.map(({text, link}, i) => (
                     <NavLink to={link} key={i}>{text}</NavLink>
                 ))}
